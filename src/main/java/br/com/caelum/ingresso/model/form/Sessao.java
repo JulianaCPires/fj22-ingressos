689567e2b150f48fabaa789aeaa68a07892d9bc0
Juliana Pires <juh.cpires2@gmail.com>
2021-04-29 22:26:38 -0300

    Criada a Classe SessaoDao

--- src/main/java/br/com/caelum/ingresso/model/form/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/form/Sessao.java
@@ -2,20 +2,38 @@ package br.com.caelum.ingresso.model.form;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 
-public class Sessao {
+@Entity
+public class Sessao{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer ID;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
+	//OnlyHibernate
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+	}
+	
+	public Sessao() {
+		
 	}
 	
 	public Integer getID() {
